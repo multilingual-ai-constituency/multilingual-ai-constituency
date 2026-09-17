cbf4e3a2a035172b69ef8d91ae4c8d47f0529257
Sabeela04 <sabeelanasar44@gmail.com>
2026-09-17 22:52:43 +0530

    Change displayed grievance information

--- js/app.js
+++ js/app.js
@@ -6163,7 +6163,7 @@ function renderCitizenGrievanceForm() {
                 ${getDashboardGrievances().slice(0, 4).map((grievance) => `
                     <div class="table-row">
                         <strong>${grievance.id}</strong>
-                        <span>${grievance.subject}</span>
+                       <span>${grievance.problem}</span>
                         <span>${grievance.category}</span>
                         <span>${grievance.status}</span>
                         <button type="button" class="detail-button" data-grievance-id="${grievance.id}">Open</button>
